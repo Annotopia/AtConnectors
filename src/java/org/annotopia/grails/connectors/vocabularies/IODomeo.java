ec52651c3f1844334b9f07600240076e9ebe65d4
Paolo Ciccarese <paolo.ciccarese@gmail.com>
2014-09-25 10:23:54 -0400

    Fixed wrong property.

--- src/java/org/annotopia/grails/connectors/vocabularies/IODomeo.java
+++ src/java/org/annotopia/grails/connectors/vocabularies/IODomeo.java
@@ -25,7 +25,7 @@ package org.annotopia.grails.connectors.vocabularies;
  */
 public interface IODomeo {
 
-	static final String agents = "domeo:agent";
+	static final String agents = "domeo:agents";
 	static final String resources = "domeo:resource";
 	static final String annotations = "domeo:annotation";
 }
